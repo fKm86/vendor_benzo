138181dd94a071af62c76d564f564a8b2ba5ff3c
mydongistiny <jaysonedson@gmail.com>
2019-09-14 14:13:02 -0700

    benzo: Add apns, keyboard swipe lib and bootanimations
    Change-Id: I23c44619dff1dd48420fc7a07f470ff1875af59e
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -4,6 +4,14 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/benzo/overlay
 # Inherit art options
 include vendor/benzo/config/art.mk
 
+# Theme Picker
+PRODUCT_PACKAGES += \
+    ThemePicker
+
+# LatinIME gesture lib
+PRODUCT_COPY_FILES += \
+    vendor/benzo/prebuilt/product/lib64/libjni_latinimegoogle.so:$(TARGET_COPY_OUT_PRODUCT)/lib64/libjni_latinimegoogle.so
+
 # Use ccache
 USE_CCACHE := true
 
