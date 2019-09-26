7c7e24859d5461131eb79acb95c09ffcea41557b
mydongistiny <jaysonedson@gmail.com>
2019-09-25 21:21:06 -0700

    benzo: set build thumbprint for all devices
    Change-Id: I2145c6a4daa02ab6270fade120de2876892c0f18
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- config/props.mk
+++ config/props.mk
@@ -1,3 +1,7 @@
 # Build fingerprint
 ADDITIONAL_BUILD_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
+
+# Build thumbprint
+ADDITIONAL_BUILD_PROPERTIES += \
+    ro.build.thumbprint=$(BUILD_THUMBPRINT)
