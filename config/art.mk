d767213e3d1232c13b81f9c2068df28c8695dde6
mydongistiny <jaysonedson@gmail.com>
2019-10-19 15:55:07 -0700

    art: Move product dexopt options to vendor
    Moved from build/make/target/product/runtime_libart.mk
    
    Change-Id: I21a8c2d9ddd2dd226f170ae779443ca6e839225e
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- config/art.mk
+++ config/art.mk
@@ -11,3 +11,16 @@ WITH_DEXPREOPT := true
 WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
 PRODUCT_DEX_PREOPT_PROFILE_DIR := vendor/dexopt_profiles
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := speed-profile
+
+# Dexopt boot types
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    pm.dexopt.first-boot=extract \
+    pm.dexopt.boot=verify
+
+# Dexopt filters
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    pm.dexopt.install=speed-profile \
+    pm.dexopt.bg-dexopt=speed-profile \
+    pm.dexopt.ab-ota=speed-profile \
+    pm.dexopt.inactive=verify \
+    pm.dexopt.shared=speed
