478a2fa03bfe6c6a5f94d19c1a50bd61508b98bc
fKm <fmetzner86@gmail.com>
2020-02-29 10:03:34 +0100

    Adapt changes to Pixel 4 XL too

--- products/benzo_coral.mk
+++ products/benzo_coral.mk
@@ -1,25 +1,39 @@
+#
+# Copyright (C) 2020 Benzo Rom
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 # Release name
 PRODUCT_RELEASE_NAME := Pixel4XL
 TARGET_DEVICE := coral
 
-# Inherit some common stuff.
+# Inherit Benzo common setup.
 $(call inherit-product, vendor/benzo/config/common_full_phone.mk)
-
 # Inherit device configuration
 $(call inherit-product, device/google/coral/aosp_coral.mk)
-
 # Inherit vendor configuration
 $(call inherit-product, vendor/google/coral/coral-vendor.mk)
-
 # Google Apps
 $(call inherit-product, vendor/googleapps/googleapps.mk)
 
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := coral
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
 PRODUCT_NAME := benzo_coral
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 4 XL
 PRODUCT_MANUFACTURER := Google
+PRODUCT_DEVICE := coral
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=coral \
@@ -28,10 +42,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT="google/coral/coral:10/QQ1B.200205.002/6084387:user/release-keys"
 BUILD_THUMBPRINT="10/QQ1B.200205.002/6084387:user/release-keys"
 
-# Copy device specific prebuilt files.
+# Copy over bootanimation.
 PRODUCT_COPY_FILES += \
-    vendor/benzo/prebuilt/product/etc/apns-full-conf.xml:product/etc/apns-conf.xml \
-	vendor/google_devices/crosshatch/product/media/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip:google \
-	vendor/google_devices/crosshatch/product/media/bootanimation-dark.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation-dark.zip:google
+    vendor/google_devices/crosshatch/product/media/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip:google \
+    vendor/google_devices/crosshatch/product/media/bootanimation-dark.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation-dark.zip:google
 
 BOARD_PREBUILT_VENDORIMAGE := vendor/images/coral/vendor.img
